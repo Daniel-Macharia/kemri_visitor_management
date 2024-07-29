4107bad171bee5fb8d94789853e7751493164dfa
Daniel macharia <ndungudanny444@gmail.com>
2024-07-29 17:15:26 +0300

    resolved bug that threw MysqlIntegrityKeyContraints upon staff check in or out

---
+++ KEMRI_Visitors_project.java
@@ -0,0 +1,272 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package kemri_visitors_project;
+
+import java.io.File;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.time.LocalTime;
+import java.util.Date;
+import java.util.Scanner;
+import javafx.application.Application;
+import javafx.fxml.FXMLLoader;
+import javafx.geometry.Rectangle2D;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.image.Image;
+import javafx.stage.Screen;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+
+/**
+ *
+ * @author Daniel Macharia
+ */
+public class KEMRI_Visitors_project extends Application {
+    
+    @Override
+    public void start(Stage stage) {
+        
+        try
+        {
+            DateNumber n = new DateNumber();
+            Parent root = FXMLLoader.load(getClass().getResource("Login.fxml"));
+            Scene scene = new Scene(root);
+            
+            stage.getIcons().add( new Image( getClass().getResourceAsStream("assets/KEMRI.png") ) );
+            stage.setScene(scene);
+            stage.setTitle("KENYA MEDICAL RESEARCH INSTITUTE VISITOR MANAGEMENT");
+
+            Screen screen = Screen.getPrimary();
+            Rectangle2D bounds = screen.getVisualBounds();
+
+            stage.setX(bounds.getMinX());
+            stage.setY(bounds.getMinY());
+            stage.setWidth(bounds.getWidth());
+            stage.setHeight(bounds.getHeight());
+
+            stage.show();
+        }catch( Exception e )
+        {
+            UtilityClass.alert("Error: " + e);
+        }
+    }
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
+
+
+class DateNumber
+{
+    private Date d;
+    private int date, year;
+    private int month;
+    
+    public DateNumber()
+    {
+        d = new Date();
+        parse();
+    }
+    
+    private int getMonth( String month )
+    {
+        String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+        
+        for( int i = 0; i < months.length; i++ )
+        {
+            if( month.equals( months[i] ) )
+                return i + 1;
+        }
+        
+        return 0;
+    }
+    
+    private void parse()
+    {
+        Scanner s = new Scanner( this.d.toString() );
+        
+        String y, m, d;
+        
+        s.next();
+        m = s.next();
+        d = s.next();
+        s.next();
+        s.next();
+        y = s.next();
+        
+        String date = y + " " + m + " " + d;
+        year = Integer.parseInt( y );
+        month = getMonth( m );
+        this.date = Integer.parseInt( d );
+        
+        if( greaterThan( getStoredDate(false) ) )
+        {
+            //update date and init UtilityClass.visitCount to 0
+            updateDBDate();
+        }
+        
+    }
+    
+    public static void incrementDBCount( Boolean staff_count)
+    {
+        try
+        {
+            Connection con;
+            PreparedStatement state;
+            String sql;
+            if( staff_count )
+            {
+                sql = "UPDATE DATE_NUMBER SET CURRENT_COUNT = ? WHERE IS_STAFF_COUNT='YES'";
+                StaffCheckInOrOut.visitCount++;
+                con = UtilityClass.getDatabaseConnection();
+
+                state = con.prepareStatement(sql);
+
+                state.setInt(1, StaffCheckInOrOut.visitCount );
+            }
+            else
+            {
+                sql = "UPDATE DATE_NUMBER SET CURRENT_COUNT = ? WHERE IS_STAFF_COUNT='NO'";
+                UtilityClass.visitCount++;
+                con = UtilityClass.getDatabaseConnection();
+
+                state = con.prepareStatement(sql);
+
+                state.setInt(1, UtilityClass.visitCount );
+            }
+
+
+            
+            int rows = state.executeUpdate();
+            
+            if( rows != 1 )
+            {
+                UtilityClass.alert("Could not update number!");
+            }
+        }catch( Exception e )
+        {
+            UtilityClass.alert("Error: " + e);
+        }
+    }
+
+    private boolean greaterThan( String storedDate )
+    {
+        String y, m, d;
+        
+        if( storedDate != null )
+        {
+            Scanner s = new Scanner( storedDate );
+        
+            y = s.next();
+            m = s.next();
+            d = s.next();
+            
+            int yr, mn, dt;
+            yr = Integer.parseInt(y);
+            mn = Integer.parseInt( m );
+            dt = Integer.parseInt( d );
+            
+            if( yr < year )
+            {
+                return true;
+            }
+            else//check month
+            {
+                if( mn < month )
+                {
+                    return true;
+                }
+                else//check date
+                {
+                    if( dt < date )
+                    {
+                        return true;
+                    }
+                }
+            }
+        }
+        
+        
+        
+        return false;
+    }
+    
+    private String getStoredDate( boolean isStaff)
+    {
+        try
+        {
+            String sql;
+            if( isStaff )
+            {
+                sql = "SELECT * FROM DATE_NUMBER WHERE IS_STAFF_COUNT='YES'";
+            }
+            else
+            {
+                sql = "SELECT * FROM DATE_NUMBER WHERE IS_STAFF_COUNT='NO'";
+            }
+            
+            Connection con = UtilityClass.getDatabaseConnection();
+            
+            PreparedStatement state = con.prepareStatement( sql );
+            
+            ResultSet result = state.executeQuery();
+            if( result.first() )
+            {
+                String num, date;
+                
+                date = result.getString(2);
+                if( isStaff )
+                {
+                    StaffCheckInOrOut.visitCount = result.getInt(3);
+                }else
+                {
+                    UtilityClass.visitCount = result.getInt(3);
+                }
+                
+                return date;
+            }
+            
+            
+        }catch( Exception e )
+        {
+            UtilityClass.alert("Error: " + e);
+        }
+        return null;
+    }
+    
+    private void updateDBDate()
+    {
+        try
+        {
+            String sql = "UPDATE DATE_NUMBER SET DATE_TODAY = ?,  CURRENT_COUNT = ?";
+            
+            Connection con = UtilityClass.getDatabaseConnection();
+            
+            PreparedStatement state = con.prepareStatement(sql);
+            
+            state.setString( 1, year + " " + month + " " + date );
+            state.setInt( 2, 0);
+            
+            int rows = state.executeUpdate();
+            
+            if( rows != 2 )
+            {
+                UtilityClass.alert("Invalid Date!");
+            }
+            
+        }catch( Exception e )
+        {
+            UtilityClass.alert("Error: " + e);
+        }
+    }
+}
---
+++ StaffCheckInOrOut.java
@@ -0,0 +1,277 @@
+package kemri_visitors_project;
+
+import com.mysql.jdbc.Util;
+import com.mysql.jdbc.exceptions.jdbc4.MySQLIntegrityConstraintViolationException;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.control.TextField;
+
+import java.net.URL;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.Date;
+import java.util.ResourceBundle;
+import java.util.Scanner;
+
+public class StaffCheckInOrOut implements Initializable {
+    public static int visitCount = 0;
+    @FXML private TextField staffNumber;
+    @Override
+    public void initialize(URL url, ResourceBundle bundle )
+    {
+        //init class members
+        visitCount = getVisitCount();
+    }
+    
+    private int getVisitCount()
+    {
+        int count = 0;
+        
+        try{
+            String sql = "SELECT * FROM DATE_NUMBER WHERE IS_STAFF_COUNT='YES'";
+            
+            Connection con = UtilityClass.getDatabaseConnection();
+            
+            PreparedStatement state = con.prepareStatement( sql );
+            
+            ResultSet result = state.executeQuery();
+            if( result.first() )
+            {
+                count = result.getInt(3);
+            }
+        }catch( Exception e )
+        {
+            UtilityClass.alert("Error: " + e);
+        }
+        
+        return count;
+    }
+
+    @FXML
+    public void handleStaffCheckInOrOutAction(ActionEvent event)
+    {
+        try
+        {
+            String number = staffNumber.getText();
+
+            if( isValidStaffNumber( number ) )
+            {
+                if( isCheckedIn( number ) )
+                {
+                    checkOut( number, false );
+                }
+                else
+                {
+                    checkIn( number );
+                }
+            }
+            else
+            {
+                if( number.matches("^[0-9]{1,3}$") )
+                {
+                    checkOut( number, true );
+                }
+                else
+                {
+                    UtilityClass.alert("Invalid staff number!");
+                }
+            }
+            
+            clearStaffNumber();
+            
+        }catch( Exception e )
+        {
+            UtilityClass.alert("Error: " + e);
+        }
+    }
+    private void clearStaffNumber()
+    {
+        staffNumber.clear();
+    }
+
+    private boolean isValidStaffNumber( String staffNumber )
+    {
+        if( UtilityClass.isValidStaffNumber( staffNumber ) )
+        {
+            return true;
+        }
+        return false;
+    }
+
+    private int getMonth( String month )
+    {
+        String[] months = {"Jan", "Feb", "Mar", "Apr", "May", "Jun", "Jul", "Aug", "Sep", "Oct", "Nov", "Dec"};
+
+        for( int i = 0; i < months.length; i++ )
+        {
+            if( month.equals( months[i] ) )
+                return i + 1;
+        }
+
+        return 0;
+    }
+
+    private String getDateString()
+    {
+        Date date = new Date();
+        Scanner s = new Scanner( date.toString() );
+
+        String y, m, d;
+
+        s.next();
+        m = getMonth( s.next() ) + "";
+        d = s.next();
+        s.next();
+        s.next();
+        y = s.next();
+
+        String dateString = y + "-" + m + "-" + d;
+
+        return dateString;
+    }
+    
+    private String getTimeString()
+    {
+        Date date = new Date();
+        Scanner s = new Scanner( date.toString() );
+
+        String hr, min, sec;
+        
+        for( int i = 0; i < 3; i++ )
+            s.next();
+        
+        String timeString = s.next();
+        
+        
+        return timeString;
+    }
+
+    private boolean isCheckedIn( String staffNumber )
+    {
+        try{
+            String dateString = getDateString();
+            String sql = "SELECT STAFF_CHECK_IN_TIME, STAFF_CHECK_OUT_TIME " +
+                    "FROM STAFF_VISIT " +
+                    "WHERE VISIT_DATE=? AND STAFF_ID=?";
+            Connection con = UtilityClass.getDatabaseConnection();
+            PreparedStatement state = con.prepareStatement( sql );
+            state.setString(1, dateString);
+            state.setString(2, staffNumber);
+
+            ResultSet result = state.executeQuery();
+
+            while( result.next() )//an entry exists
+            {
+                if( result.getString(2).equals("") )
+                {
+                    return true;
+                }
+            }
+        }catch ( Exception e )
+        {
+            UtilityClass.alert("Error: " + e );
+        }
+        return false;
+    }
+
+    private void checkIn( String staffNumber )
+    {
+        try
+        {
+            
+            String time_out = "";
+            String dateString = getDateString();
+            String check_in_time = getTimeString();
+            
+            String sql = "INSERT INTO STAFF_VISIT VALUES (?, ?, ?, ?, ?, ?)";
+            
+            Connection con = UtilityClass.getDatabaseConnection();
+            
+            PreparedStatement state = con.prepareStatement(sql);
+            
+            state.setString(1, dateString);
+            state.setInt(2, StaffCheckInOrOut.visitCount + 1);
+            state.setString(3, staffNumber);
+            state.setString(4, "KEMRI");
+            state.setString(5, check_in_time);
+            state.setString(6, time_out);
+            
+            
+            int rows = state.executeUpdate();
+            
+            if( rows == 1 )
+            {
+                UtilityClass.inform("Successfully checked in.");
+                DateNumber.incrementDBCount(true);
+            }
+            
+        }catch(MySQLIntegrityConstraintViolationException e )
+        {
+            UtilityClass.alert("Not a registered staff!\nPlease consult the admin.");
+        }catch( Exception e )
+        {
+            UtilityClass.alert("Error: " + e);
+        }
+    }
+    
+    private boolean isCheckedOut()
+    {
+        //check whether the staff was already checked
+        
+        return false;
+    }
+
+    private void checkOut( String staffNumber, boolean isVisitNumber )
+    {
+        if( isCheckedOut() )
+        {
+            return;//no checking out twice
+        }
+        try
+        {
+            String dateString = getDateString();
+            String outTime = getTimeString();
+
+            String sql = "UPDATE STAFF_VISIT " +
+                    "SET STAFF_CHECK_OUT_TIME=? " +
+                    "WHERE VISIT_DATE=? AND STAFF_ID=?";
+            
+            String sql2 = "UPDATE STAFF_VISIT " +
+                    "SET STAFF_CHECK_OUT_TIME=? " +
+                    "WHERE VISIT_DATE=? AND VISIT_NUMBER=?";
+            
+            Connection con = UtilityClass.getDatabaseConnection();
+            
+            PreparedStatement state;
+            if( isVisitNumber ) 
+            {
+                state = con.prepareStatement( sql2 );
+            }
+            else
+            {
+                state = con.prepareStatement( sql );
+            }
+            
+            state.setString(1, outTime);
+            state.setString(2, dateString );
+            state.setString(3, staffNumber);
+
+            int rows = state.executeUpdate();
+
+            if( rows > 0 )
+            {
+                UtilityClass.inform("Successfully checked out.");
+            }
+            else
+            {
+                UtilityClass.alert("Could not check out. Rows: " + rows );
+            }
+        }catch( Exception e )
+        {
+            UtilityClass.alert("Error: " + e);
+        }
+    }
+
+}
